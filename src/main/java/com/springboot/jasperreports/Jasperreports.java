fab11ba85d2344654dc13a903f0832dbe3503bf5
jerry691 <jerrywang6886@gmail.com>
2020-07-20 09:35:16 -0400

    initial commit

---
+++ src/main/java/com/springboot/jasperreports/Jasperreports.java
@@ -0,0 +1,16 @@
+package com.springboot.jasperreports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main implementation class which serves two purpose in a spring boot application: Configuration and bootstrapping.
+ * @author nanda
+ */
+@SpringBootApplication
+public class Jasperreports {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jasperreports.class, args);
+	}
+}
---
+++ src/main/java/com/springboot/jasperreports/service/EmployeeService.java
@@ -0,0 +1,14 @@
+package com.springboot.jasperreports.service;
+
+import java.util.List;
+
+import com.springboot.jasperreports.model.Employee;
+
+/**
+ * @author nanda
+ *
+ */
+public interface EmployeeService {
+
+	List<Employee> findAll();
+}
